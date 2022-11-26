99c48522bc39ca0b0dd199e993ece398766a5b75
hiroto-yk <105357836+hiroto-yk@users.noreply.github.com>
2022-11-26 19:40:50 +0900

    update

--- api/api.php
+++ api/api.php
@@ -52,10 +52,10 @@ switch ($command) {
     break;
 }
 
-$cmd = '/home/t190d117/.opam/default/bin/dk ' . $command . $option . $tmpfile . ' 2>&1';
+$cmd = '/home/hiroto/.opam/default/bin/dk ' . $command . $option . $tmpfile . ' 2>&1';
 
 echo $command . "<br>";
-echo "<font color=\"red\">&gt; " . $cmd . "</font><br>";
+echo "<font color=\"green\">&gt; " . $cmd . "</font><br>";
 exec($cmd, $output, $result_code); //実行
 exec('rm ./tmp/tempfile.dk');
 
